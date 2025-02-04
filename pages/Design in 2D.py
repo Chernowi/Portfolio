0e6b3c6e1e38fd5a92d4d0a3b2a685f01320f0a4
Pedro Agundez Fernandez <chernowi@gmail.com>
2025-02-04 17:11:50 +0100

    changed image width

--- pages/Design in 2D.py
+++ pages/Design in 2D.py
@@ -7,7 +7,7 @@ st.write("""I started diving into 2D/graphic design when I installed Adobe Illus
 
 col1, col2, col3 = st.columns(3)
 
-col1.image("images/2d_design/gvlogo.jpg", width=300, caption="Logo for my climbing association")
-col2.image("images/2d_design/cami.png", width=300, caption="Design for a t-shirt")
-col3.image("images/2d_design/pezleon.png", width=300, caption="Tattoo design of a lionfish")
+col1.image("images/2d_design/gvlogo.jpg", width=220, caption="Logo for my climbing association")
+col2.image("images/2d_design/cami.png", width=220, caption="Design for a t-shirt")
+col3.image("images/2d_design/pezleon.png", width=220, caption="Tattoo design of a lionfish")
 
